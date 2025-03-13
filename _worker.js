990e6f43865c128589a45174b8ad981930168d6f
Kwak David <196980578+kwak-david@users.noreply.github.com>
2025-03-14 06:02:10 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ import { connect } from "cloudflare:sockets";
 
 // Variables
 const rootDomain = "seaguu.my.id"; // Ganti dengan domain utama kalian
-const serviceName = "project"; // Ganti dengan nama workers kalian
+const serviceName = "my"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
